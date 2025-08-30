fd17bb989aa9c847f769f879e1dc5d84f26666c9
juewx <maximo12221899@gmail.com>
2025-08-30 11:06:11 -0400

    grupo 26 xd
    este es el codigo de segimiento de linia

---
+++ equipo 26/maid.py
@@ -0,0 +1,14 @@
+import cyberpi
+
+while True:
+    if cyberpi.quad_rgb_sensor.is_line("L1",1) == True and cyberpi.quad_rgb_sensor.is_line("R1",1) == True:
+        cyberpi.mbot2.drive_power(30, -30)
+    #si ve a la por la izquierda perro
+    elif cyberpi.quad_rgb_sensor.is_line("L1",1) == True and cyberpi.quad_rgb_sensor.is_line("R1",1)== False:
+     cyberpi.mbot2.drive_power(0,-30)
+    #hola
+    elif cyberpi.quad_rgb_sensor.is_line("L1",1) == False and cyberpi.quad_rgb_sensor.is_line("R1",1) == True:
+        cyberpi.mbot2.drive_power(30, -0)
+    #aaaaa
+    elif cyberpi.quad_rgb_sensor.is_line("L1",1) == False and cyberpi.quad_rgb_sensor.is_line("R1",1) == False:
+        cyberpi.mbot2.drive_power(-30,30)    
