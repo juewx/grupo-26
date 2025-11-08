5c45aa1a62d3267a8b3304057a747b3323d402aa
juewx <maximo12221899@gmail.com>
2025-11-08 20:09:19 -0300

    este si es el codigo

--- maid.py
+++ maid.py
@@ -1,27 +1,91 @@
-import  cyberpi
 import time
-import detesion_de_color
+import cyberpi
 
-while True:
-    if cyberpi.quad_rgb_sensor.is_line("L1",1) == True and cyberpi.quad_rgb_sensor.is_line("R1",1) == True:
-        cyberpi.mbot2.drive_power(30, -30)
-    #si ve a la por la izquierda perro
-    elif cyberpi.quad_rgb_sensor.is_line("L1",1) == True and cyberpi.quad_rgb_sensor.is_line("R1",1)== False:
-     cyberpi.mbot2.drive_power(0,-30)
-    #hola
-    elif cyberpi.quad_rgb_sensor.is_line("L1",1) == False and cyberpi.quad_rgb_sensor.is_line("R1",1) == True:
-        cyberpi.mbot2.drive_power(30, -0)
-    #aaaaa
-    elif cyberpi.quad_rgb_sensor.is_line("L1",1) == False and cyberpi.quad_rgb_sensor.is_line("R1",1) == False:
-        cyberpi.mbot2.drive_power(-30,30)
+def linea(velocidad_base):
+    offset = cyberpi.quad_rgb_sensor.get_offset_track(1)
+    velocidad_motor_derecha = -velocidad_base + (-offset * 0.5)
+    velocidad_motor_izquierda = velocidad_base + (-offset * 0.6)
+    cyberpi.mbot2.drive_speed(velocidad_motor_izquierda, velocidad_motor_derecha)
 
-def main():
-    # Solo sigue la línea si detecta rojo
-    if detesion_de_color.detectar_color():
-        # Aquí va el código para seguir la línea
-        # Por ejemplo:
-        print("¡Detectado rojo! Siguiendo la línea...")
-        # Tu lógica de movimiento aquí
-    else:
-        print("No hay rojo. Detenido.")
-        # Tu lógica para detener el robot aquí
+@cyberpi.event.is_press("a")
+def press_a():
+    while True:
+        baño()
+        
+        grutita()
+        cancer()
+        despertar()
+        linea(25)
+
+def baño():
+    sen_izquierda = cyberpi.quad_rgb_sensor.is_color("b", "L1", 1)
+    sen_derecha = cyberpi.quad_rgb_sensor.is_color("b", "R1", 1)
+
+    if sen_derecha or sen_izquierda:
+        cyberpi.mbot2.drive_power(0, 0)
+        time.sleep(0.2)
+        cyberpi.mbot2.turn(5, 10)
+        time.sleep(0.3)
+        cyberpi.mbot2.servo_set(90, "s1")
+        time.sleep(0.2)
+        cyberpi.mbot2.servo_set(170, "s1")
+        time.sleep(0.3)
+        cyberpi.mbot2.servo_set(90,"s1")
+        time.sleep(0.2)
+        cyberpi.mbot2.servo_set(170, "s1")
+        time.sleep(0.2)
+        cyberpi.mbot2.servo_set(90, "s1")
+        time.sleep(0.2)
+        cyberpi.mbot2.drive_power(-40,40)
+        time.sleep(0.8)
+        cyberpi.mbot2.turn(-90, 25)
+        time.sleep(0.2)
+
+def cancer():
+    sen_izquierda = cyberpi.quad_rgb_sensor.is_color("y", "L1", 1)
+    sen_derecha = cyberpi.quad_rgb_sensor.is_color("y", "R1", 1)
+
+    if sen_derecha == True or sen_izquierda == True:
+    
+     cyberpi.mbot2.turn(85,-10)    
+     time.sleep(0.1)               
+     
+
+def grutita():
+    sen_izquierda = cyberpi.quad_rgb_sensor.is_color("g", "L1", 1)
+    sen_derecha = cyberpi.quad_rgb_sensor.is_color("g", "R1", 1)
+
+    if sen_derecha == True or sen_izquierda == True:
+        cyberpi.mbot2.drive_power(20,-20)
+        time.sleep(0.5)
+        cyberpi.mbot2.servo_set(92,"s3")
+        time.sleep(0.2)
+        cyberpi.mbot2.servo_set(70,"s3")
+        time.sleep(0.2)
+        cyberpi.mbot2.drive_power(-10,10)
+        time.sleep(1.2)
+        cyberpi.mbot2.turn(-120,20)
+        time.sleep(0.1)
+        cyberpi.mbot2.drive_power(20,-20)
+        time.sleep(1.0)
+        cyberpi.mbot2.drive_power(0,0)
+        time.sleep(120)
+
+def despertar():
+    sen_izquierda = cyberpi.quad_rgb_sensor.is_color("r", "L1", 1)
+    sen_derecha = cyberpi.quad_rgb_sensor.is_color("r", "R1", 1)
+
+    if sen_derecha == True or sen_izquierda == True:
+        cyberpi.mbot2.turn(60,20)
+        time.sleep(0.1)
+        cyberpi.mbot2.turn(-30,20)
+        time.sleep(0.1)
+        cyberpi.mbot2.turn(30,10)
+        time.sleep(0.1)
+        cyberpi.mbot2.servo_set(90, "s1")
+        time.sleep(0.2)
+        cyberpi.mbot2.servo_set(90, "s3")
+        time.sleep(0.2)
+        cyberpi.mbot2.straight(3,40)
+        time.sleep(0.1)
+        cyberpi.mbot2.turn(-10,8)
